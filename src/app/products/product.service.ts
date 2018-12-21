ba3cdd741f8be73d912de4e832ca59efefc79cbb
Michal Karwowski <michal.karwowski.0@gmail.com>
2018-12-21 16:22:01 +0100

    reformat kodu, zmiana endpointu

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -12,7 +12,7 @@ export class ProductService {
   productDeleted = new Subject<Product>();
   products: Product[];
 
-  apiEndpoint = 'http://localhost:9000';
+  apiEndpoint = 'http://localhost:9000/products';
 
 
   constructor(private authService: AuthService,
@@ -23,7 +23,7 @@ export class ProductService {
     // return this.products;
     // const req = new HttpRequest('GET', this.authService.apiEndpoint + 'product');
 
-    return this.httpClient.get<Product[]>(this.apiEndpoint + '/products');
+    return this.httpClient.get<Product[]>(this.apiEndpoint);
     //   .pipe(map(
     //     (products) => {
     //       return products;
@@ -36,11 +36,18 @@ export class ProductService {
     //   );
   }
 
+  addProduct(product: Product) {
+    // this.products.push(product);
+    const req = new HttpRequest('POST', this.apiEndpoint, product);
+
+    return this.httpClient.request(req);
+  }
+
   getProduct(id: number) {
     // return this.products[id];
     // return this.products.find(x => x.id === id);
 
-    return this.httpClient.get<Product>(this.apiEndpoint + '/products/' + id);
+    return this.httpClient.get<Product>(`${this.apiEndpoint}/${id}`);
   }
 
   updateProduct(id: number, product: Product) {
@@ -48,16 +55,9 @@ export class ProductService {
     // const prodID = product.id;
     // const req = new HttpRequest('POST', this.authService.apiEndpoint + 'product/' + prodID, product);
     // return this.httpClient.request(req);
-    const req = this.httpClient.post(this.apiEndpoint + '/products/' + product.id, product);
+    const req = this.httpClient.put(`${this.apiEndpoint}/${product.id}`, product);
     this.productsChanged.next(product);
     return req;
-  }
-
-  addProduct(product: Product) {
-    // this.products.push(product);
-    const req = new HttpRequest('POST', this.apiEndpoint + '/products/add', product);
-
-    return this.httpClient.request(req);
   }
 
   deleteProduct(product: Product) {
